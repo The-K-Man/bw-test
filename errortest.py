fb1659a13b8e55dd8e7751235bb4b240da8ed14a
kyriellp <kyriell.paris-agafonov@ccur.com>
2016-06-06 13:56:24 -0400

    Add existing file

---
+++ errortest.py
@@ -0,0 +1,13 @@
+import unittest
+from bwtest import human_read
+
+class Test(unittest.TestCase):
+    def test_basic_addition(self):
+        self.failUnlessEqual(human_read(1), "1.00Bps")
+        self.failUnlessEqual(human_read(2000), "1.00KBps")
+        self.failUnlessEqual(human_read(2500000), "2.00MBps")
+
+
+if __name__ == '__main__':
+	unittest.main()
+
