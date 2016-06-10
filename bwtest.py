3ac384f2c1dba24c0e36cc94df039e67c47165b1
kyriellp <kyriell.paris-agafonov@ccur.com>
2016-06-10 14:45:37 -0400

    add coveralls to travis.yml

--- bwtest.py
+++ bwtest.py
@@ -3,6 +3,73 @@ import time
 # Creates a list of all interfaces to assure the user used the correct input
 
 
+class curr_prev_holder(object):
+    #prevs
+    prevBI = 0
+    prevBO = 0
+    prevPI = 0
+    prevPO = 0
+    prevEI = 0
+    prevEO = 0
+    #currs
+    bytes_in = 0
+    bytes_out = 0
+    packet_out = 0
+    packet_in = 0
+    errors_in = 0
+    errors_out = 0
+
+
+    def __init__(self, interface):
+        self.interface = interface
+
+
+# Makes data in Bytes human readable
+    def human_read(self, data):
+        data_sizes = ['Bps', 'KBps', 'MBps', 'GBps', 'TBps']
+        if (data) == 0:
+            return '0Bps'
+        i = 0
+        while data >= 1024 and i < len(data_sizes) - 1:
+                data /= 1024
+                i += 1
+        new_number = ('%.2f' % data)
+        return str(str(new_number) + str(data_sizes[i]))
+
+
+    def set_prvs(self, arr):
+        self.prevBI = int(arr[1])
+        self.prevBO = int(arr[9])
+        self.prevPI = int(arr[2])
+        self.prevPO = int(arr[10])
+        self.prevEI = int(arr[3])
+        self.prevEO = int(arr[11])
+
+
+    def set_currs(self, arr):
+        self.bytes_in = self.human_read(int(arr[1]) - self.prevBI)
+        self.bytes_out = self.human_read(int(arr[9]) - self.prevBO)
+        self.packet_in = self.human_read(int(arr[2]) - self.prevPI)
+        self.packet_out = self.human_read(int(arr[10]) - self.prevPO)
+        self.errors_in = self.human_read(int(arr[3]) - self.prevEI)
+        self.errors_out = self.human_read(int(arr[11]) - self.prevEO)
+
+
+    def new_dev_info(self):
+        return str((' ' *
+                   len(self.interface) + '  ') + self.bytes_in +
+                   ' ' * (12 - len(self.bytes_in)) +
+                   self.packet_in + ' ' *
+                   (14-len(self.packet_in)) +
+                   self.errors_in +
+                   ' ' * (10-len(self.errors_in)) +
+                   self.bytes_out + ' ' *
+                   (12-len(self.bytes_out)) +
+                   self.packet_out + ' ' *
+                   (14-len(self.packet_out)) +
+                   self.errors_out)
+
+
 def interfaces():
     with open('/proc/net/dev', 'r') as dev_file:
         interface_list = []
@@ -14,7 +81,7 @@ def interfaces():
 
 # Prints the header
 def header(name):
-    header_tuple = (str(name + 
+    header_tuple = (str(name +
                     " |   Receive                         |  Transmit"),
                     str(" " * len(name) +
                     " |bytes       packets       errs     |bytes       "+
@@ -22,31 +89,13 @@ def header(name):
     return header_tuple
 
 
-# Makes data in Bytes human readable
-def human_read(data):
-    data_sizes = ['Bps', 'KBps', 'MBps', 'GBps', 'TBps']
-    if (data) == 0:
-        return '0Bps'
-    i = 0
-    while data >= 1024 and i < len(data_sizes) - 1:
-            data /= 1024
-            i += 1
-    new_number = ('%.2f' % data)
-    return str(str(new_number) + str(data_sizes[i]))
-
-
 def main_bw_test():
-    prevBI = 0
-    prevBO = 0
-    prevPI = 0
-    prevPO = 0
-    prevEI = 0
-    prevEO = 0
     times_run = 0
     if len(sys.argv) != 2:
         print("Number of arguments incorrect")
         sys.exit()
     devname = str(sys.argv[1])
+    bw_info = curr_prev_holder(devname)
     while True:
         try:
             with open('/proc/net/dev', 'r') as dev_file:
@@ -65,33 +114,11 @@ def main_bw_test():
                                     new_tup =  header(name)
                                     print new_tup[0]
                                     print new_tup[1]
-                                bytes_in = human_read(int(arr[1]) - prevBI)
-                                bytes_out = human_read(int(arr[9]) - prevBO)
-                                packet_in = human_read(int(arr[2]) - prevPI)
-                                packet_out = human_read(int(arr[10]) - prevPO)
-                                errors_in = human_read(int(arr[3]) - prevEI)
-                                errors_out = human_read(int(arr[11]) - prevEO)
-                                # 66 chars in the header after the initial |.
-                                dev_info = str((' ' *
-                                               len(name) + '  ') + bytes_in +
-                                               ' ' * (12 - len(bytes_in)) +
-                                               packet_in + ' ' *
-                                               (14-len(packet_in)) +
-                                               errors_in +
-                                               ' ' * (10-len(errors_in)) +
-                                               bytes_out + ' ' *
-                                               (12-len(bytes_out)) +
-                                               packet_out + ' ' *
-                                               (14-len(packet_out)) +
-                                               errors_out)
+                                 # 66 chars in the header after the initial |.
+                                bw_info.set_currs(arr)
+                                dev_info = bw_info.new_dev_info()
                                 print (dev_info)
-                                # Set prev sizes equal to curr sizes
-                                prevBI = int(arr[1])
-                                prevBO = int(arr[9])
-                                prevPI = int(arr[2])
-                                prevPO = int(arr[10])
-                                prevEI = int(arr[3])
-                                prevEO = int(arr[11])
+                                bw_info.set_prvs(arr)
                                 times_run += 1
                                 time.sleep(1)
                     except:
@@ -100,6 +127,7 @@ def main_bw_test():
         except IOError:
             print("'/proc/net/dev' not available. ")
             sys.exit()
+
 
 if __name__ == "__main__":
    main_bw_test()
--- errortest.py
+++ errortest.py
@@ -1,14 +1,9 @@
 import sys
 import unittest
-from bwtest import human_read, interfaces, header
+from bwtest import interfaces, header
+from bwtest import curr_prev_holder
 
 class Test(unittest.TestCase):
-    
-    def test_human_read(self):
-        self.failUnlessEqual(human_read(1), "1.00Bps")
-        self.failUnlessEqual(human_read(2000), "1.00KBps")
-        self.failUnlessEqual(human_read(2500000), "2.00MBps")
-        self.failUnlessEqual(human_read(0), "0Bps")
 
 
     def test_interfaces(self):
@@ -22,14 +17,34 @@ class Test(unittest.TestCase):
                     str(" " * len(eno1) +
                     " |bytes       packets       errs     |bytes       "+
                     "packets       errs")))
-        self.failUnlessEqual(header(eno1), (test_tup))
+        self.failUnlessEqual(header("eno1"), (test_tup))
 
 
-    #def test_main(self):
-        #self.assertRaises(IOError, main_bw_test())
+    def test_class_human(self):
+        x = curr_prev_holder("eno1")
+        self.failUnlessEqual(x.human_read(1), "1.00Bps")
+        self.failUnlessEqual(x.human_read(2000), "1.00KBps")
+        self.failUnlessEqual(x.human_read(2500000), "2.00MBps")
+        self.failUnlessEqual(x.human_read(0), "0Bps")
+    
+
+    def test_class(self):
+        arr = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14]
+        x = curr_prev_holder("eno1")
+        x.set_currs(arr)
+	x.set_prvs(arr)
+        self.failUnlessEqual(x.prevPO, 10)
+        self.failUnlessEqual(x.bytes_in, "1.00Bps")
+
+        
+    def test_printer(self):
+       arr = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14]
+       x = curr_prev_holder("eno1")
+       x.set_currs(arr)
+       x.set_prvs(arr)
+       self.failUnlessEqual(x.new_dev_info(), "      1.00Bps     2.00Bps       3.00Bps   9.00Bps     10.00Bps      11.00Bps")
+
 
 
 if __name__ == '__main__':
-    for i in sys.argv:
-        print i
     unittest.main()
